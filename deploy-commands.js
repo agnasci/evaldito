6c7e2b7e5294bea6e08b527a4ca732ecf7d3f1d3
Matheus Aguiar do Nascimento <mathscndev@gmail.com>
2023-07-14 17:05:20 -0300

    Initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,43 @@
+const { REST, Routes } = require('discord.js');
+
+// DOTENV:
+
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID } = process.env;
+
+// IMPORT COMMANDS:
+
+const fs = require('node:fs');
+const path = require('node:path');
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const commands = [];
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// INSTANCE REST
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// DEPLOY
+
+(async () => {
+	try {
+		console.log(`Resetando ${commands.length} comando(s)...`);
+
+		await rest.put(
+			Routes.applicationCommands(CLIENT_ID),
+			{ body: commands },
+		);
+
+		console.log('Comandos registrados com sucesso!');
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
